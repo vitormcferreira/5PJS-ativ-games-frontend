49fcfd77df295e095ce81fbca9588fea3c2a19b0
Vitor <vitorpgmdr@gmail.com>
2021-11-18 00:23:08 -0300

    estilizar Container

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -11,5 +11,11 @@ export default createGlobalStyle`
 `;
 
 export const Container = styled.section`
-  color: ${colors.primaryDarkColor};
+  max-width: 480px;
+  overflow-x: auto;
+  background-color: ${colors.primarylightColor};
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 `;
