ebb9dc9eded666e9eda441b561a3c210dda8a1a9
Vitor <vitorpgmdr@gmail.com>
2021-11-18 00:24:21 -0300

    remover botaoClicado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { FaHome, FaUserAlt, FaSignInAlt } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector(
-    (state) => state.exampleReducer.botaoClicado
-  );
   return (
     <Nav>
       <Link to="/">
@@ -21,7 +17,6 @@ export default function Header() {
       <Link to="/b">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não clicado'}
     </Nav>
   );
 }
