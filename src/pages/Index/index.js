0a27da71b0b9242e680d9ce5fc01f4bc249a7cb1
Vitor <>
2021-12-05 03:43:45 -0300

    editar Index

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
-import { Container } from '../../styles/GlobalStyles';
+import { useDispatch } from 'react-redux';
+import { Container, Botoes } from './styled';
 import JogoDaMemoria from '../../components/JogoDaMemoria';
+import Movimentos from '../../components/Movimentos';
 import Ranking from '../../components/Ranking';
+import Header from '../../components/Header';
+import * as actions from '../../store/modules/jogo/actions';
 
 export default function Index() {
+  const dispatch = useDispatch();
+
+  const handleReiniciar = (e) => {
+    e.preventDefault();
+    dispatch(actions.novoJogoRequest({ reiniciar: true }));
+  };
+
+  React.useEffect(() => {
+    dispatch(actions.novoJogoRequest());
+  }, [dispatch]);
+
   return (
     <Container>
+      <Header />
+      <Movimentos />
       <Ranking />
       <JogoDaMemoria />
+      <Botoes>
+        <button type="button">Mudar tema</button>
+        <button type="button" onClick={handleReiniciar}>
+          Reiniciar
+        </button>
+      </Botoes>
     </Container>
   );
 }
--- src/pages/Index/styled.js
+++ src/pages/Index/styled.js
@@ -1,1 +1,17 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: grid;
+  grid-template-areas:
+    'header     header'
+    'movimentos jogo-da-memoria'
+    'ranking    jogo-da-memoria'
+    'botoes     jogo-da-memoria';
+
+  grid-template-rows: 6vh 10vh 64vh 20vh;
+  grid-template-columns: 1fr 4fr;
+`;
+
+export const Botoes = styled.div`
+  grid-area: botoes;
+`;
